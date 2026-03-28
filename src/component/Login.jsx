09aa4077b3d8af0b4ade28a3cf53499fc0e56949
Charles prince chinecherem <charlesprincechinecherem@gmail.com>
2026-03-29 00:19:14 +0100

    Update alert message on login failure
    Change alert message for unsuccessful login attempt.

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -71,7 +71,7 @@ export default function Login() {
             alert("Login successful ✅");
             navigate("/");
         } catch (err) {
-            alert(err.message);
+            alert("Login Unsuccessful);
         }
     };
 

612ec350b7930d22e3f062739a2c7cbb97692125
Charles prince chinecherem <charlesprincechinecherem@gmail.com>
2026-03-31 21:03:03 +0100

    Implement login functionality in Login component
    Refactor Login component to uncomment and implement login functionality with password visibility toggle.

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,57 +1,3 @@
-// import { useState } from "react";
-// import { useAuth } from "../context/AuthContext";
-// import { useNavigate } from "react-router-dom";
-// import { HiEye, HiEyeOff } from 'react-icons/hi'
-
-// export default function Login() {
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
-//     const [matched, setmatched] = useState(false)
-//     const { login } = useAuth();
-//     const navigate = useNavigate();
-
-//     const handleLogin = async () => {
-//         try {
-//             await login(email, password);
-//             alert("Login successful ✅");
-//             navigate("/");
-//         } catch (err) {
-//             alert(err.message);
-//         }
-//     };
-
-//     return (
-//         <div className="flex flex-col items-center mt-20">
-//             <h2 className="text-2xl font-bold mb-4">Login</h2>
-
-//             <input
-//                 type="email"
-//                 placeholder="Email"
-//                 className="border p-2 mb-2"
-//                 onChange={(e) => setEmail(e.target.value)}
-//             />
-//             <span>
-
-//                 <input
-//                     type={matched ? 'text' : "password"}
-//                     placeholder="Password"
-//                     className="border p-2 mb-2"
-//                     onChange={(e) => setPassword(e.target.value)}
-//                 />
-//                 <button onClick={() => setmatched(!matched)}>
-//                     {matched ? <HiEye size={28} /> : <HiEyeOff size={28} />}
-//                 </button>
-//             </span>
-
-//             <button
-//                 onClick={handleLogin}
-//                 className="bg-blue-500 text-white px-4 py-2"
-//             >
-//                 Login
-//             </button>
-//         </div>
-//     );
-// }
 
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
@@ -116,7 +62,7 @@ export default function Login() {
                         onClick={() => setShowPassword(!showPassword)}
                         className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-600"
                     >
-                        {showPassword ? <HiEye size={22} /> : <HiEyeOff size={22} />}
+                        {showPassword ? <HiEyeOff size={22} /> : <HiEye size={22} />}
                     </button>
                 </div>
 
